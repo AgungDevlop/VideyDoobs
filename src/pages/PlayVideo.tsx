e70bcb81ec5e76e47d687431756f32c11caf46de
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2025-06-03 01:59:59 +0800

    Update PlayVideo.tsx

--- src/pages/PlayVideo.tsx
+++ src/pages/PlayVideo.tsx
@@ -22,7 +22,7 @@ export function PlayVideo() {
   const videosPerPage = 10;
 
   // Array URL video dasar
-  const videoBaseUrls = ['https://doobs.top/e/'];
+  const videoBaseUrls = ['https://videy.doobs.top/e/'];
 
   // Array URL untuk pop-under
   const randomUrls = [
