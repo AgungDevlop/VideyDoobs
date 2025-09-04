08a41e41bbec259963c99ccb0165b7921d2abe31
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2025-09-05 03:40:19 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -6,6 +6,7 @@ export function Download() {
 
   // Array of URLs for pop-under links
   const randomUrls = [
+    "https://adclickad.com/get/?spot_id=6089412&cat=25&subid=2067093145",
     "https://enviousgarbage.com/HE9TFh",
     "https://aviatorreproducesauciness.com/2082665",
     "https://superficial-work.com/ba3RV.0YPk3Xp/v/b/mOVsJHZqDV0Y0KO/DVQWzkOvD/MK3pLvT/QJ4JNmDyM/4MMozHgS"
