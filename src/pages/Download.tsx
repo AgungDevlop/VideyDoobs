8a551a6de49c130254e2c55fcc5da6da601d1d9f
AgungDevlop <142754535+AgungDevlop@users.noreply.github.com>
2026-07-25 02:27:35 +0800

    Update Download.tsx

--- src/pages/Download.tsx
+++ src/pages/Download.tsx
@@ -5,10 +5,8 @@ export function Download() {
   const videoTitle = sessionStorage.getItem('videoTitle'); // Get video title from session storage
 
 const randomUrls = [
-    'https://otieu.com/4/10055984',
-    'https://smart-damage.com/HE9TFh',
-    'https://aviatorreproducesauciness.com/2082665'
-];
+    'https://dulyhagglermounting.com/2082665',
+    ];
 
   const handleDownload = () => {
     if (videoUrl) {
